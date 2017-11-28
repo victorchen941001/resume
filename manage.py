1c9463f02748ac5c109d87e3116f9a0b89ec9833
Jude Chen <victorchen941001@gmail.com>
2017-11-27 19:01:23 -0500

    new_resume

--- manage.py
+++ manage.py
@@ -1,7 +1,26 @@
 from flask_script import Manager
-from resume import app
+from resume import app, db, Professor, Course
 
 manager = Manager(app)
+
+
+
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    professor1 = Professor(name='Jiang,Hao', department='ECON')
+    professor2 = Professor(name='Sharma,Pratyush Nidhi', department='MISY')
+    course1 = Course(course_number="MISY330",title='Database Design and Implementation', description='Design and implementation of enterprise databases in the business environment', professor=professor2)
+    course2 = Course(course_number="ECON308",title='Banking and Monetary Policy', description='Nature and economic significance of money, credit and the banking system', professor=professor1)
+    course3 = Course(course_number="NTDT200",title='Nutrition Concepts', description='Functions and sources of nutrients, dietary adequacy, energy balance and metabolism with emphasis on health promotion.')
+    course4 = Course(course_number="ART180",title='Digital Photography ', description='Introduces the basics of photography as a way to communicate ideas emphasizing content, composition, and technique.')
+    db.session.add(professor2)
+    db.session.add(course1)
+    db.session.add(course2)
+    db.session.add(course3)
+    db.session.add(course4)
+    db.session.commit()
 
 
 if __name__ == '__main__':
--- resume.py
+++ resume.py
@@ -1,25 +1,117 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, session, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+# define database tables
+class Professor(db.Model):
+    __tablename__ = 'professors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    department = db.Column(db.String(64))
+    courses = db.relationship('Course', backref='professor')
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    course_number = db.Column(db.String(60))
+    title = db.Column(db.String(256))
+    description = db.Column(db.Text)
+    professor_id = db.Column(db.Integer, db.ForeignKey('professors.id'))
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
-
-
-@app.route('/courses')
-def show_all_courses():
-    courses = [
-        'MISY350',
-        'MISY330',
-        'MISY160'
-    ]
-    return render_template('courses.html',courses=courses)
-
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+# Professor
+@app.route('/professors')
+def professors():
+    professors = Professor.query.all()
+    return render_template('professors.html', professors=professors)
+
+@app.route('/professor/add', methods=['GET', 'POST'])
+def add_professors():
+    if request.method == 'GET':
+        return render_template('professor-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        department = request.form['department']
+
+        # insert the data into the database
+        professor = Professor(name=name, department=department)
+        db.session.add(professor)
+        db.session.commit()
+
+        return redirect(url_for('professors'))
+
+@app.route('/professor/edit/<int:id>', methods=['GET', 'POST'])
+def edit_professor(id):
+    professor= Professor.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return render_template('professor-edit.html', professor=professor)
+    if request.method == 'POST':
+        # update data based on the form data
+        professor.name = request.form['name']
+        professor.department = request.form['department']
+        # update the database
+        db.session.commit()
+        return redirect(url_for('professors'))
+
+# Course
+@app.route('/courses')
+def courses():
+    courses = Course.query.all()
+    return render_template('courses.html', courses=courses)
+
+@app.route('/course/add', methods=['GET', 'POST'])
+def add_courses():
+    if request.method == 'GET':
+        professors = Professor.query.all()
+        return render_template('course-add.html', courses=courses, professors=professors)
+
+    if request.method == 'POST':
+        # get data from the form
+        course_number = request.form['course_number']
+        title = request.form['title']
+        description = request.form['description']
+        professor_name = request.form['professor']
+        professor = Professor.query.filter_by(name=professor_name).first()
+        course = Course(course_number=course_number,title=title, description=description)
+
+        # insert the data into the database
+        db.session.add(course)
+        db.session.commit()
+        return redirect(url_for('courses'))
+
+
+@app.route('/course/edit/<int:id>', methods=['GET', 'POST'])
+def edit_course(id):
+    course = Course.query.filter_by(id=id).first()
+    professors = Professor.query.all()
+    if request.method == 'GET':
+        return render_template('course-edit.html', course=course, professors=professors)
+    if request.method == 'POST':
+        # update data based on the form data
+        course_number = request.form['course_number']
+        course.title = request.form['title']
+        course.description = request.form['description']
+        professor_name = request.form['professor']
+        professor = Professor.query.filter_by(name=professor_name).first()
+        course.professor = professor
+        # update the database
+        db.session.commit()
+        return redirect(url_for('course'))
 
 if __name__ == '__main__':
     app.run(debug=True)
